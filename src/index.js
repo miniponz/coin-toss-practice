870fa5e251a3f51fb9bb6494b62d0f6c6b9e1481
Cara Ponzini <ponzini@gmail.com>
2019-01-30 14:18:55 -0800

    working cat dog counter

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import catOrDog from './cat-or-dog.js';
 const animalForm = document.getElementById('animal-form');
 const image = document.getElementById('animal-image');
 const result = document.getElementById('result');
+const catCount = document.getElementById('cat-count');
+const dogCount = document.getElementById('dog-count');
+console.log(dogCount, catCount);
+
+let cats = 0;
+let dogs = 0;
 
 
 animalForm.addEventListener('submit', function(event){
@@ -19,10 +25,15 @@ animalForm.addEventListener('submit', function(event){
     if(animal === 'cat'){
         imageSource = './assets/cat.jpg';
         result.textContent = 'You\'re a cat person!';
+        cats++;
+        catCount.textContent = 'Cat Count: ' + cats;
     }
     else {
         imageSource = './assets/dog.jpg';
         result.textContent = 'You\'re a dog person!';
+        dogs++;
+        dogCount.textContent = 'Dog Count: ' + dogs;
+        
     }
 
     console.log(imageSource);
